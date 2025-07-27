7e121e65def7995c3def263f016513accbecdaea
Plator-JS <plator.osmani@hotmail.com>
2025-07-27 16:01:28 +0200

    Add files via upload

---
+++ IT_quiz.py
@@ -0,0 +1,83 @@
+print("Was für eine Distribution ist Ubuntu?")
+print("1.Windows")
+print("2.Linux")
+print("3.Mac OS")
+print("4.Temple OS")
+
+antwort1 = input("Ihre Eingabe: ")
+if antwort1 == "2" or antwort1 == "Linux" or antwort1 == "2.Linux":
+    print("Richtig!")
+else:
+    print("Falsch! Richtige Antwort: 2.Linux")
+
+print("\nWas ist das beste Betriebssystem zum Hacken?")
+print("1.Mint Linux")
+print("2.Mac OS")
+print("3.Windows")
+print("4.Kali Linux")
+
+antwort2 = input("Ihre Eingabe: ")
+if antwort2 == "4" or antwort2 == "Kali Linux" or antwort2 == "4.Kali Linux":
+    print("Richtig!")
+else:
+    print("Falsch! Richtige Antwort: 4.Kali Linux")
+
+print("\nMit welchem Browser kommt man am besten ins Darknet?")
+print("1.Tor")
+print("2.Google")
+print("3.Microsoft Edge")
+print("4.Firefox")
+
+antwort3 = input("Ihre Eingabe: ")
+if antwort3 == "1" or antwort3 == "Tor" or antwort3 == "1.Tor":
+    print("Richtig!")
+else:
+    print("Falsch! Richtige Antwort: 1.Tor")
+
+print("\nMit was enden alle Darknet Links?")
+print("1..ch")
+print("2..onion")
+print("3..net")
+print("4..com")
+
+antwort4 = input("Ihre Eingabe: ")
+if antwort4 == "2" or antwort4 == ".onion" or antwort4 == "2..onion":
+    print("Richtig!")
+else:
+    print("Falsch! Richtige Antwort: 2..onion")
+
+print("\nWas bedeutet die Abkürzung CPU?")
+print("1.Central Process Unit")
+print("2.Central Processing Unit")
+print("3.Computer Processing Unit")
+print("4.Core Processing Unit")
+
+antwort5 = input("Ihre Eingabe: ")
+if antwort5 == "3" or antwort5 == "Computer Processing Unit" or antwort5 == "3.Computer Processing Unit":
+    print("Richtig!")
+else:
+    print("Falsch! Richtige Antwort: 3.Computer Processing Unit")
+
+print("\nWelcher Befehl ist eine Funktion in Python?")
+print("1.def()")
+print("2.import")
+print("3.if")
+print("4.try")
+
+antwort6 = input("Ihre Eingabe: ")
+if antwort6 == "1" or antwort6 == "def()" or antwort6 == "1.def()":
+    print("Richtig!")
+else:
+    print("Falsch! Richtige Antwort: 1.def()")
+
+print("\nWas ist der Hauptzweck von einer Firewall?")
+print("1.Daten speichern")
+print("2.Den Computer schneller machen")
+print("3.Viren entfernen")
+print("4.Unerwünschte Netzwerkverbindungen blockieren")
+
+antwort7 = input("Ihre Eingabe: ")
+if antwort7 == "4" or antwort7 == "Unerwünschte Netzwerkverbindungen blockieren" or antwort7 == "4.Unerwünschte Netzwerkverbindungen blockieren":
+    print("Richtig!")
+else:
+    print("Falsch! Richtige Antwort: 4.Unerwünschte Netzwerkverbindungen blockieren")
